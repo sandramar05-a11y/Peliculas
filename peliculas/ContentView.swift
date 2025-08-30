de6afadc2623681eec3160ccfc412242f9097420
Sandra <sandra.mar05@iest.edu.mx>
2025-08-29 18:42:52 -0600

    Initial Commit

---
+++ peliculas/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  peliculas
+//
+//  Created by WIN603 on 29/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ peliculas/peliculasApp.swift
@@ -0,0 +1,17 @@
+//
+//  peliculasApp.swift
+//  peliculas
+//
+//  Created by WIN603 on 29/08/25.
+//
+
+import SwiftUI
+
+@main
+struct peliculasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
