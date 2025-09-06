987115eb55a9edd34bf37002e406581951a237d2
Sandra <sandra.mar05@iest.edu.mx>
2025-09-05 18:43:11 -0600

    peliculas

---
+++ peliculas/Peliculas.swift
@@ -0,0 +1,18 @@
+//
+//  Peliculas.swift
+//  peliculas
+//
+//  Created by WIN603 on 01/09/25.
+//
+
+import SwiftUI
+
+struct Peliculas: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Peliculas()
+}
---
+++ peliculas/practica.swift
@@ -0,0 +1,18 @@
+//
+//  practica.swift
+//  peliculas
+//
+//  Created by WIN603 on 05/09/25.
+//
+
+import SwiftUI
+
+struct practica: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    practica()
+}
